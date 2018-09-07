3bcb2d3e900931b9d5f688ad7f5b8e2c3dd45f21
prithvish-doshi-17 <43064854+prithvish-doshi-17@users.noreply.github.com>
2018-09-07 17:07:17 +0530

    Update logic.py
    Only the main logic remaining. Need to write a while loop and we are done.

--- logic.py
+++ logic.py
@@ -1,1 +1,25 @@
+m = input("Enter the capacity of smaller jug:                                  ")     #Taking inputs
+n = input("Enter the capacity of larger jug:                                   ")
+c = input("Enter the required capacity:                                        ")
+m = int(m)
+n = int(n)
+c = int(c)
 
+if (c>m and c>n):                                                                     #Checking error condition
+	print("Error")
+
+def gcd(m,n):                                                                         #Function to find gcd
+	if(n%m == 0):
+		return m
+	else:
+		return gcd(n%m,m)
+
+a = gcd(m,n)
+print(a)
+
+if(c%a != 0):                                                                         #Checking existence of solution
+	print("Solution does not exist")
+	return
+
+x = 0                                                                                 #x denotes current amount in m
+y = 0                                                                                 #y denotes current amount in n
