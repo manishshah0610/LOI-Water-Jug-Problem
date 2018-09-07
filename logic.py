ccea6d04a6e4cbfab9d70d59b4c89e07cbed6f98
prithvish-doshi-17 <43064854+prithvish-doshi-17@users.noreply.github.com>
2018-09-07 17:29:30 +0530

    Update logic.py
    The while loop is giving false outputs but the outputs are not completely random. Try to find errors.

--- logic.py
+++ logic.py
@@ -5,8 +5,9 @@ m = int(m)
 n = int(n)
 c = int(c)
 
-if (c>m and c>n):                                                                     #Checking error condition
+if (c>n or m>n):                                                                      #Checking error condition
 	print("Error")
+	exit(0)
 
 def gcd(m,n):                                                                         #Function to find gcd
 	if(n%m == 0):
@@ -15,11 +16,18 @@ def gcd(m,n):                                                                   
 		return gcd(n%m,m)
 
 a = gcd(m,n)
-print(a)
 
 if(c%a != 0):                                                                         #Checking existence of solution
 	print("Solution does not exist")
-	return
 
-x = 0                                                                                 #x denotes current amount in m
-y = 0                                                                                 #y denotes current amount in n
+else:
+	x = 0                                                                         #x denotes current amount in m
+	y = 0                                                                         #y denotes current amount in n
+
+	while(x != c and y!=c):
+		if(x==0):
+			x = x + n
+		else:
+			x = (x - (m-y))
+		y = (y + (n-x))
+		print (x,y)
