949b39738865dea29ca1f408778f41f4c695cff6
shreya1706 <shreyasagar1706@gmail.com>
2018-09-11 15:03:58 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,74 +1,74 @@
-m = input("Enter the capacity of smaller jug:                                  ")     #Taking inputs
-n = input("Enter the capacity of larger jug:                                   ")
-c = input("Enter the required capacity:                                        ")
-m = int(m)                                                                            #Converting strings to integers
-n = int(n)
-c = int(c)
+small_jug= input("Enter the capacity of smaller jug:                                  ")     #Taking inputs
+large_jug = input("Enter the capacity of larger jug:                                   ")
+amount_required = input("Enter the required capacity:                                        ")
+small_jug = int(small_jug)                                                                            #Converting strings to integers
+large_jug  = int(large_jug)
+amount_required = int(amount_required)
 
-if (c>n or m>n):                                                                      #Checking error condition
+if (amount_required > large_jug or small_jug > large_jug):                                                 #Checking error condition
 	print("Error")
 	exit(0)
 
-def gcd(m,n):                                                                         #Function to find gcd
-	if(n%m == 0):
-		return m
+def gcd(small_jug,large_jug):                                                                         #Function to find gcd
+	if(large_jug % small_jug == 0):
+		return small_jug
 	else:
-		return gcd(n%m,m)
+		return gcd(large_jug % small_jug,small_jug)
 
-a = gcd(m,n)
+a = gcd(small_jug,large_jug)
 
-if(c%a != 0):                                                                         #Checking existence of solution
+if(amount_required % a != 0):                                                                         #Checking existence of solution
 	print("Solution does not exist")
 	exit(0)
 
-def solution(fromcap, tocap, d):                                                      #Function to find optimal solution
-	f = fromcap
-	t = 0
+def solution(fromcap, tocap, amount):                                                      #Function to find optimal solution
+	from_jug = fromcap
+	to_jug = 0
 	step = 0
 	step += 1
-	while (f != d and t != d):
-		temp = min(f, tocap - t)
-		t += temp
-		f -= temp
+	while (from_jug != amount and to_jug != amount):
+		temp = min(from_jug, tocap - t)
+		to_jug += temp
+		from_jug -= temp
 		step += 1
 
-		if(f == d or t == d):
+		if(from_jug == amount or to_jug == amount):
 			break
 
-		if (f == 0):
-			f = fromcap
+		if (from_jug == 0):
+			from_jug = fromcap
 
-		if (t == tocap):
+		if (to_jug == tocap):
 			t = 0
 
 		step += 1
 	return step
 
-def printSolution(fromcap, tocap, d):                                                  #Function to print optimal solution
-	f = fromcap
-	t = 0
-	print(f,t)
-	while (f != d and t != d):
-		temp = min(f, tocap - t)
-		t += temp
-		f -= temp
-		print(f, t)
+def printSolution(fromcap, tocap, amount):                                                  #Function to print optimal solution
+	from_jug = fromcap
+	to_jug = 0
+	print(from_jug,to_jug)
+	while (from_jug != amount and to_jug != amount):
+		temp = min(from_jug, tocap - to_jug)
+		to_jug += temp
+		from_jug -= temp
+		print(from_jug, to_jug)
 
-		if(f == d or t == d):
+		if(from_jug == amount or to_jug == amount):
 			break
 
-		if (f == 0):
-			f = fromcap
+		if (from_jug == 0):
+			from_jug = fromcap
 
-		if (t == tocap):
-			t = 0
+		if (to_jug == tocap):
+			to_jug = 0
 
-		print(f, t)
+		print(from_jug, to_jug)
 
-m1 = solution(m,n,c)
-m2 = solution(n,m,c)
+m1 = solution(small_jug,large_jug,amount_required)
+m2 = solution(large_jug,small_jug,amount_required)
 
 if(m1<m2):
-	printSolution(m,n,c)
+	printSolution(small_jug,large_jug,amount_required)
 else:
-	printSolution(n,m,c)
+	printSolution(large_jug,small_jug,amount_required)
